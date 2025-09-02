b2f9fc94bcb4477f6e0d0f809f5bb5a2d04f7c81
Artworqq <keysub.kevin@gmail.com>
2025-09-02 17:42:43 -0400

    Create card_library.js

---
+++ public/assets/card_library.js
@@ -0,0 +1,25 @@
+// card_library.js
+export const CardLibrary = [
+  {
+    id: "000057",
+    name: "KEVIN SUBER IS ME IM AM  #1",
+    emojis: ["🌍", "📰"], // ✅ FIXED
+    about: "YouTube",
+    tribute: "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
+    effects: [
+      { icons: "🌍📰", emoji: "🌍", text: "By DOOM" },
+      { icons: "🌍📰", emoji: "🌍", text: "Adult Swim Bumps | #1" }
+    ],
+    atk: 4200,
+    def: 3360,
+    level: 8,
+    tributes: 8,
+    rarity: "UR",
+    card_sets: ["YouTube", "2025 YouTube"],
+    card_images: [
+      {
+        image_url: "https://i.ytimg.com/vi/KNCJTC_thLA/hqdefault.jpg"
+      }
+    ]
+  }
+];
