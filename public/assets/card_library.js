b4f6b660fa4706b6fff40e2d878d7b0ea25ea70e
Artworqq <keysub.kevin@gmail.com>
2025-09-02 19:59:57 -0400

    Update card_library.js

--- public/assets/card_library.js
+++ public/assets/card_library.js
@@ -1,25 +1,55 @@
 // card_library.js
 export const CardLibrary = [
-  {
-    id: "000057",
-    name: "KEVIN SUBER IS ME IM AM  #1",
-    emojis: ["🌍", "📰"], // ✅ FIXED
-    about: "YouTube",
-    tribute: "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
-    effects: [
-      { icons: "🌍📰", emoji: "🌍", text: "By DOOM" },
-      { icons: "🌍📰", emoji: "🌍", text: "Adult Swim Bumps | #1" }
-    ],
-    atk: 4200,
-    def: 3360,
-    level: 8,
-    tributes: 8,
-    rarity: "UR",
-    card_sets: ["YouTube", "2025 YouTube"],
-    card_images: [
-      {
-        image_url: "https://i.ytimg.com/vi/KNCJTC_thLA/hqdefault.jpg"
-      }
-    ]
-  }
+{
+  "id": "000173",
+  "name": "Deep Blue Sea",
+  "icon": "🩺🧬",
+  "about": "YouTube",
+  "tribute": "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
+  "effects": [
+    {
+      "icons": "🩺🧬",
+      "emoji": "🩺",
+      "text": "How fast can you swim?A beautiful scientist is on the verge of making medical history. But for the greater good, her experiments have violated ethical codes ..."
+    },
+    {
+      "icons": "🩺🧬",
+      "emoji": "🩺",
+      "text": "Deep Blue Sea"
+    }
+  ],
+  "atk": 2600,
+  "def": 2080,
+  "level": 4,
+  "tributes": 4,
+  "rarity": "SR",
+  "tags": [
+    "video",
+    "sharing",
+    "camera phone",
+    "video phone",
+    "free",
+    "upload",
+    "#businesscoaching",
+    "#entrepreneurial",
+    "#businessmindset",
+    "#sales",
+    "#startup"
+  ],
+  "card_sets": [
+    "YouTube",
+    "2025 YouTube"
+  ],
+  "timestamp": "2025-09-02T23:09:42.330Z",
+  "footer": "Jessica AI • SPZ | Zetsumetsu Eoe™ | ZETSUMETSU CORPORATION | Artworqq Kevin Suber",
+  "card_images": [
+    {
+      "image_url": "https://i.ytimg.com/vi/g4oKAIANCvU/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGBkgSih_MA8=&amp;rs=AOn4CLDZXC-fYRo-ajPGwRgiiWlXm5sotA"
+    }
+  ],
+  "frameType": "health",
+  "category": "Health",
+  "color": "red-orange",
+  "_source_url": "https://youtu.be/g4oKAIANCvU?si=PL3mT8PP8wapUrTq"
+}
 ];
