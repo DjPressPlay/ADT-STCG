4e31c025c47ef29416d4030d02946524533ac1e9
Artworqq <keysub.kevin@gmail.com>
2025-09-03 20:59:09 -0400

    Create deck.js

---
+++ public/pages/deck.js
@@ -0,0 +1,50 @@
+// deck.js
+export const Deck = [
+  {
+    "id": "000044",
+    "name": "Future Tools - Blink",
+    "icon": "🏅🏟️",
+    "about": "futuretools.io",
+    "tribute": "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
+    "effects": [
+      {
+        "icons": "🏅🏟️",
+        "emoji": "🏅",
+        "text": "No description available"
+      },
+      {
+        "icons": "🏅🏟️",
+        "emoji": "🏅",
+        "text": "Future Tools - Blink"
+      }
+    ],
+    "atk": 3000,
+    "def": 2400,
+    "level": 5,
+    "tributes": 5,
+    "rarity": "R",
+    "tags": [
+      "#entrepreneurlife",
+      "#selfemployed",
+      "#opportunity",
+      "#startups",
+      "#goals"
+    ],
+    "card_sets": [
+      "futuretools.io",
+      "2025 futuretools.io"
+    ],
+    "timestamp": "2025-09-04T00:49:48.860Z",
+    "footer": "Jessica AI • SPZ | Zetsumetsu Eoe™ | ZETSUMETSU CORPORATION | Artworqq Kevin Suber",
+    "card_images": [
+      {
+        "image_url": "https://cdn.prod.website-files.com/639910cce4b41d2a9b3e96fb/6407c9da316ad90928888b90_favicon.png"
+      }
+    ],
+    "frameType": "sports",
+    "category": "Sports",
+    "color": "green",
+    "_source_url": "https://www.futuretools.io/tools/blink"
+  }
+  // ⬅️ Add more card objects here
+];
