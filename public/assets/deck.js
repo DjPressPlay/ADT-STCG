6d9113ab6ea2081251c408fbe585869a9a0bdb1a
Artworqq <keysub.kevin@gmail.com>
2025-09-04 02:24:10 -0400

    Update deck.js

--- public/assets/deck.js
+++ public/assets/deck.js
@@ -1,50 +1,56 @@
 // deck.js
 export const Deck = [
-  {
-    "id": "000044",
-    "name": "Future Tools - Blink",
-    "icon": "🏅🏟️",
-    "about": "futuretools.io",
-    "tribute": "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
-    "effects": [
-      {
-        "icons": "🏅🏟️",
-        "emoji": "🏅",
-        "text": "No description available"
-      },
-      {
-        "icons": "🏅🏟️",
-        "emoji": "🏅",
-        "text": "Future Tools - Blink"
-      }
-    ],
-    "atk": 3000,
-    "def": 2400,
-    "level": 5,
-    "tributes": 5,
-    "rarity": "R",
-    "tags": [
-      "#entrepreneurlife",
-      "#selfemployed",
-      "#opportunity",
-      "#startups",
-      "#goals"
-    ],
-    "card_sets": [
-      "futuretools.io",
-      "2025 futuretools.io"
-    ],
-    "timestamp": "2025-09-04T00:49:48.860Z",
-    "footer": "Jessica AI • SPZ | Zetsumetsu Eoe™ | ZETSUMETSU CORPORATION | Artworqq Kevin Suber",
-    "card_images": [
-      {
-        "image_url": "https://cdn.prod.website-files.com/639910cce4b41d2a9b3e96fb/6407c9da316ad90928888b90_favicon.png"
-      }
-    ],
-    "frameType": "sports",
-    "category": "Sports",
-    "color": "green",
-    "_source_url": "https://www.futuretools.io/tools/blink"
-  }
+{
+  "id": "000197",
+  "name": "Resident Evil: Degeneration",
+  "icon": "⚖️🏛️",
+  "about": "YouTube",
+  "tribute": "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
+  "effects": [
+    {
+      "icons": "⚖️🏛️",
+      "emoji": "⚖️",
+      "text": "The &quot;Umbrella Incident&quot; that took place in Raccoon City was ended with the eradication of the deadly virus by a missile attack ordered by the government. 200..."
+    },
+    {
+      "icons": "⚖️🏛️",
+      "emoji": "⚖️",
+      "text": "Resident Evil: Degeneration"
+    }
+  ],
+  "atk": 5000,
+  "def": 4000,
+  "level": 10,
+  "tributes": 10,
+  "rarity": "UR",
+  "tags": [
+    "video",
+    "sharing",
+    "camera phone",
+    "video phone",
+    "free",
+    "upload",
+    "#ecommerce",
+    "#businessideas",
+    "#businessmotivation",
+    "#leadership",
+    "#personalbranding"
+  ],
+  "card_sets": [
+    "YouTube",
+    "2025 YouTube"
+  ],
+  "timestamp": "2025-09-04T05:39:31.877Z",
+  "footer": "Jessica AI • SPZ | Zetsumetsu Eoe™ | ZETSUMETSU CORPORATION | Artworqq Kevin Suber",
+  "card_images": [
+    {
+      "image_url": "https://i.ytimg.com/vi/O6onWEEqkgk/maxresdefault.jpg"
+    }
+  ],
+  "frameType": "government",
+  "category": "Government",
+  "color": "gray",
+  "_source_url": "https://youtu.be/O6onWEEqkgk?si=IdwdkRkJdhNu_wjr"
+}
   // ⬅️ Add more card objects here
 ];
